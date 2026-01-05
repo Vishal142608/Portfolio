dff7128bd4e8752e531ac146db205a50d8fe48e2
Vishal-1426 <pubgsaini267@gmail.com>
2026-01-05 13:55:36 +0530

    Fix db path for Railway and local

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -79,12 +79,22 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/data/db.sqlite3',
+import os
+
+if os.environ.get("RAILWAY_ENVIRONMENT"):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': '/data/db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
